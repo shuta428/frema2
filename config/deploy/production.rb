28ab49e90b4d0bd7df1d003f54bc90089519f9a7
shuta428 <shuhamu428@gmail.com>
2020-09-26 02:48:35 +0900

    basic認証

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,6 +8,8 @@ server '176.32.64.22', user: 'ec2-user', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 
 
 # role-based syntax
