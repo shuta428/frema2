b53d1688a560bc338b788e267baa2f54fc600f52
shuta428 <shuhamu428@gmail.com>
2020-09-26 03:12:24 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,8 @@
 server '176.32.64.22', user: 'ec2-user', roles: %w{app db web}
+
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -7,10 +11,6 @@ server '176.32.64.22', user: 'ec2-user', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-
-set :rails_env, "production"
-set :unicorn_rack_env, "production"
-
 
 # role-based syntax
 # ==================
