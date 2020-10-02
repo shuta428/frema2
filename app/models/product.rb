8ea577daf5367ebf15c99c38c13d7619902111b7
shuta428 <shuhamu428@gmail.com>
2020-10-03 07:07:44 +0900

    product,imageモデルの設定

---
+++ app/models/product.rb
@@ -0,0 +1,21 @@
+class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :user
+  has_many :images, dependent: :destroy
+
+  belongs_to_active_hash :shippingday
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :fee
+
+  with_options presence: true do
+  validates :name
+  validates :content
+  validates :price
+  validates :condition_id
+  validates :fee_id
+  validates :prefecture_id
+  validates :shippingday_id
+  validates :category_id
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_104715) do
+ActiveRecord::Schema.define(version: 2020_10_02_220208) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "zipcode", null: false
@@ -30,6 +30,32 @@ ActiveRecord::Schema.define(version: 2020_10_02_104715) do
     t.datetime "updated_at", null: false
     t.string "ancestry"
     t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image", null: false
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_images_on_product_id"
+  end
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "content", null: false
+    t.bigint "category_id", null: false
+    t.text "brand", null: false
+    t.integer "condition_id", null: false
+    t.integer "fee_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "shippingday_id", null: false
+    t.integer "price", null: false
+    t.integer "status", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -54,4 +80,7 @@ ActiveRecord::Schema.define(version: 2020_10_02_104715) do
   end
 
   add_foreign_key "addresses", "users"
+  add_foreign_key "images", "products"
+  add_foreign_key "products", "categories"
+  add_foreign_key "products", "users"
 end
