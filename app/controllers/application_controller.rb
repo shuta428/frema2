841b3336f5c681bfa052e55b98991b095881bdc4
shuta428 <shuhamu428@gmail.com>
2020-10-06 19:29:55 +0900

    ヘッダーのテンプレート作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,16 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth, if: :production? 
   protect_from_forgery with: :exception
+  before_action :set_category
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, :phone_number])
+  end
+
+  def set_category
+    @parents = Category.where(ancestry: nil)
   end
 
   private
