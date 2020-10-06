1cea0f7a6096234a0a07ed21f17d063a7d1a2bdb
shuta428 <shuhamu428@gmail.com>
2020-10-07 06:56:35 +0900

    product、imageモデルの修正

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   belongs_to :category
   belongs_to :user
   has_many :images, dependent: :destroy
+  accepts_nested_attributes_for :images, allow_destroy: true
 
   belongs_to_active_hash :shippingday
   belongs_to_active_hash :prefecture
