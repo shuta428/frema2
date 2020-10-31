e0be8b5d4752028469d0866485766377292b7719
shuta428 <shuhamu428@gmail.com>
2020-10-31 12:16:05 +0900

    モデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_many :address
+  has_one :card
 end
