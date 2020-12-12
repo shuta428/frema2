9ac4e9b941281fbbf873119a48c119e71cf316e3
shuta428 <shuhamu428@gmail.com>
2020-12-13 01:41:03 +0900

    登録画面までのリンク設定

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,4 @@
 class CardsController < ApplicationController
+  def new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
     get  'addresses', to: 'users/registrations#new_address'
-    get "users/index",to: "users#index"
+    get "users/show",to: "users#show"
     get "users/card", to: "users#card"
     get "users/products",to:"users#products"
     get "users/logout", to: "users#logout"
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
   root to: "products#index"
 
  
-  resources :card, only: [:new, :show] do
+  resources :cards, only: [:new, :show] do
     collection do
       post 'show', to: 'card#show'
       post 'pay', to: 'card#pay'
